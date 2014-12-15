6abdd92085652ce69a46d4bce1cec9c6901c16ca
Ben <b.cylin@gmail.com>
2014-12-16 00:32:03 +0800

    Update the bridging header

--- HTKDynamicResizingCellDemo/Sample/HTKDynamicResizingCellDemo-Bridging-Header.h
+++ HTKDynamicResizingCellDemo/Sample/HTKDynamicResizingCellDemo-Bridging-Header.h
@@ -2,6 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "HTKSampleTableViewCell.h"
-#import "HTKSampleCollectionViewCell.h"
+#import "HTKDynamicResizingCollectionViewCell.h"
+#import "HTKDynamicResizingTableViewCell.h"
 #import "HTKDynamicResizingCellProtocol.h"
